eb086f8de2c7e6041b1119f7ffb4883e5dbfac14
James Parker <jamzxb@outlook.com>
2025-08-08 22:32:50 +1000

    learned

--- structs.cpp
+++ structs.cpp
@@ -6,13 +6,15 @@ struct student{
     bool enrolled;
 };
 
-struct car
+struct Car
 {
     std::string model;
     int year;
     std::string color;
 };
 
+void printCar(Car car);
+void paintCar(Car &car, std::string color);
 
 int main(){
 
@@ -25,8 +27,30 @@ int main(){
     std::cout << student1.gpa << '\n';
     std::cout << student1.enrolled << '\n';
 
-    car car1;
-    car car2;
+    Car car1;
+    Car car2;
+
+    car1.model = "Mustang";
+    car1.year = 2023;
+    car1.color = "red";
+
+    car2.model = "Corvette";
+    car2.year = 2024;
+    car2.color = "blue";
+
+    printCar(car1);
+    paintCar(car1, "black");
+    printCar(car1);
 
     return 0;
+}
+
+void printCar(Car car){
+    std::cout << car.model << '\n';
+    std::cout << car.year << '\n';
+    std::cout << car.color << '\n';
+}
+
+void paintCar(Car &car, std::string color){
+    car.color = color;
 }
